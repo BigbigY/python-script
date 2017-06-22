82573f48ac0be6a5d5850e0e9600ae049651e1c3
luck <yy520it@gmail.com>
2017-06-22 10:38:06 +0800

    update 重启tengine排除报警

--- tegine_falcon_4xx_5xx.py
+++ tegine_falcon_4xx_5xx.py
@@ -28,24 +28,25 @@ def ChangeMerit():
 
 def TmpData():            
     with open('/tmp/curl_tmp.txt','w') as f:  # 写文件
-        for merit_k in merit.keys():  
-            f.write("%s,%s,%s,%s\n"%(merit_k,merit[merit_k][3],merit[merit_k][6],merit[merit_k][7]))
+        for merit_k in merit.keys():
+            f.write("%s,%s,%s,%s,%s\n"%(merit_k,merit[merit_k][3],merit[merit_k][6],merit[merit_k][7],merit[merit_k][0]))
 def ReadDic():
     with open('/tmp/curl_tmp.txt') as f:  # 转换字典
         for l in f.xreadlines():  
             l = l.split(',')
-            up_merit[l[0]] = l[1],l[2],l[3]
+            up_merit[l[0]] = l[1],l[2],l[3],l[4]
 len_num = LenNum()
 ChangeMerit()
+# TmpData()
 ReadDic()
 
 for merit_k in merit.keys(): # 两个字典的K一样，因此做一次循环
-    if float(merit[merit_k][3]) < 100.0:
-        continue
+    if int(merit[merit_k][0]) < int(up_merit[merit_k][3]):
+        break 
     tag = "domain=%s"%merit_k 
     
     req_total_sum,http_4xx_sum,http_5xx_sum = float(merit[merit_k][3]) - float(up_merit[merit_k][0]),float(merit[merit_k][6]) - float(up_merit[merit_k][1]),float(merit[merit_k][7]) - float(up_merit[merit_k][2])
-
+    if float(req_total_sum) < 100.0:continue # 总请求太低不计算
     http_4xx_percent = http_4xx_sum*100.0#/req_total_sum # 开始计算4xx百分比
     http_5xx_percent = http_5xx_sum*100.0#/req_total_sum # 开始计算5xx百分比
     if http_4xx_percent == 0.0:
@@ -62,6 +63,5 @@ for merit_k in merit.keys(): # 两个字典的K一样，因此做一次循环
     payload.append({"metric":"ngx_http_total","endpoint":endpoint,"timestamp":int(time.time()),"step":step,"value":req_total_sum,"counterType":counterType,"tags":tag})
     payload.append({"metric":"ngx_http_5xx_sum","endpoint":endpoint,"timestamp":int(time.time()),"step":step,"value":http_5xx_sum,"counterType":counterType,"tags":tag})
     payload.append({"metric":"ngx_http_4xx_sum","endpoint":endpoint,"timestamp":int(time.time()),"step":step,"value":http_4xx_sum,"counterType":counterType,"tags":tag})
-
 r = requests.post(furl, data=json.dumps(payload))
 TmpData() # 存1分钟前数据
